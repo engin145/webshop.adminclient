0d4fbefccc2139730ccb019a5979d5c0120c2c1f
KIKEP1990 <kickker90@gmail.com>
2013-10-29 12:51:15 +0200

    fix order

--- src/main/java/com/algo/webshop/adminclient/controller/OrderController.java
+++ src/main/java/com/algo/webshop/adminclient/controller/OrderController.java
@@ -1,10 +1,8 @@
 package com.algo.webshop.adminclient.controller;
 
-import java.sql.Date;
 import java.util.Calendar;
-import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -14,14 +12,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.algo.webshop.common.domain.Good;
-import com.algo.webshop.common.domain.GoodsList;
+import com.algo.webshop.adminclient.good.OrderFull;
 import com.algo.webshop.common.domain.Order;
-import com.algo.webshop.common.domain.Position;
-import com.algo.webshop.common.domain.Price;
 import com.algo.webshop.common.domainimpl.IOrder;
 import com.algo.webshop.common.domainimpl.IOrderGood;
 import com.algo.webshop.common.domainimpl.IPrice;
+import com.algo.webshop.common.domainimpl.IUser;
 
 @Controller
 public class OrderController {
@@ -29,6 +25,12 @@ public class OrderController {
 	private IOrder serviceOrder;
 	private IOrderGood serviceOrderGood;
 	private IPrice servicePrice;
+	private IUser serviceUser;
+
+	@Autowired
+	public void setUserService(@Qualifier("userService") IUser service) {
+		this.serviceUser = service;
+	}
 
 	@Autowired
 	public void setOrderService(@Qualifier("orderService") IOrder service) {
@@ -52,11 +54,28 @@ public class OrderController {
 	}
 
 	@RequestMapping(value = "/orders", method = RequestMethod.POST)
-	public String showOrders2(Model model, @RequestParam("date") int date,
+	public String showOrders(Model model, @RequestParam("date") int date,
 			@RequestParam("confirm_status") int confirmStatus,
 			@RequestParam("cansel_status") int canselStatus) {
-		List<Order> orderList = serviceOrder.getOrders(confirmStatus,
-				canselStatus);
+		List<Order> orders;
+		if (date == 1) {
+			Calendar dateM = Calendar.getInstance();
+			dateM.add(Calendar.MONTH, -1);
+			orders = serviceOrder.getOrdersList(confirmStatus, canselStatus,
+					dateM);
+		} else {
+			orders = serviceOrder.getOrders(confirmStatus, canselStatus);
+		}
+		List<OrderFull> orderList = new LinkedList<OrderFull>();
+		for (Order order : orders) {
+			int userId = order.getUsers_id();
+			String userName = "";
+			if (userId != 0) {
+				userName = serviceUser.getUserName(userId);
+			}
+			orderList.add(new OrderFull(order, userName));
+
+		}
 		model.addAttribute("orderList", orderList);
 		return "orders";
 	}
---
+++ src/main/webapp/WEB-INF/pages/orders.jsp
@@ -0,0 +1,76 @@
+<%@page import="com.algo.webshop.adminclient.good.OrderFull"%>
+<%@page import="com.algo.webshop.common.domain.Order"%>
+<%@page import="java.util.List"%>
+<%@page import="java.util.Calendar"%>
+<%@page import="java.text.SimpleDateFormat"%>
+<%@ page language="java" contentType="text/html; charset=utf-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="s" uri="http://www.springframework.org/tags"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Insert title here</title>
+</head>
+<style type="text/css">
+td {
+	padding: 10px;
+}
+
+table {
+	border-collapse: collapse;
+	text-align: center;
+	margin: 0 auto;
+}
+
+td {
+	border: 1px solid #000;
+}
+.info {
+	border: none;
+}
+
+</style>
+<body>
+	<c:choose>
+		<c:when test="${not empty orderList}">
+			<table>
+				<tr>
+					<td>Номер</td>
+					<td>Псевдоним</td>
+					<td>Дата заказа</td>
+					<td>Статус оплаты</td>
+					<td>Статус отправки</td>
+				</tr>
+				<%
+					List<OrderFull> orderList = (List<OrderFull>) request
+									.getAttribute("orderList");
+							for (OrderFull order : orderList) {
+								SimpleDateFormat formatter = new SimpleDateFormat(
+										"yyyy-MM-dd hh.mm.ss");
+								String time = formatter.format(order.getDate_order()
+										.getTime());
+								out.println("<tr><td><a href=fullOrder?order="+order.getNumber()+">" + order.getNumber()
+										+ "</a></td><td>" + order.getUserName()
+										+ "</td><td>" + time + "</td>");
+								if (order.getDate_pay() != null) {
+									out.println("<td><img src="+request.getContextPath()+"/resources/picture/tick.png></td>");
+								} else {
+									out.println("<td><img src="+request.getContextPath()+"/resources/picture/error.png></td>");
+								}
+								if (order.getDate_release() != null) {
+									out.println("<td><img src="+request.getContextPath()+"/resources/picture/tick.png></td>");
+								} else {
+									out.println("<td><img src="+request.getContextPath()+"/resources/picture/error.png></td>");
+								}
+		
+							}
+				%>
+			</table>
+		</c:when>
+		<c:otherwise><p/>Заказов не найдено!<p></c:otherwise>
+	</c:choose>
+</body>
+</html>
